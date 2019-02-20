3f023b52bf072172700cace67cfd6bf645dc8faa
Yash Pratap Singh <singhyashpratap06@gmail.com>
2019-02-20 10:40:24 +0530

    added exchange type 'topic'

--- emit-log-topic.js
+++ emit-log-topic.js
@@ -4,14 +4,14 @@ var amqp = require('amqplib/callback_api');
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var ex = 'direct_logs';
+    var ex = 'topic_logs';
     var args = process.argv.slice(2);
+    var key = (args.length > 0) ? args[0] : 'anonymous.info';
     var msg = args.slice(1).join(' ') || 'Hello World!';
-    var severity = (args.length > 0) ? args[0] : 'info';
 
-    ch.assertExchange(ex, 'direct', {durable: false});
-    ch.publish(ex, severity, new Buffer.from(msg));
-    console.log(" [x] Sent %s: '%s'", severity, msg);
+    ch.assertExchange(ex, 'topic', {durable: false});
+    ch.publish(ex, key, new Buffer(msg));
+    console.log(" [x] Sent %s:'%s'", key, msg);
   });
 
   setTimeout(function() { conn.close(); process.exit(0) }, 500);
--- receive-log-topic.js
+++ receive-log-topic.js
@@ -5,25 +5,25 @@ var amqp = require('amqplib/callback_api');
 var args = process.argv.slice(2);
 
 if (args.length == 0) {
-  console.log("Usage: receive_logs_direct.js [info] [warning] [error]");
+  console.log("Usage: receive_logs_topic.js <facility>.<severity>");
   process.exit(1);
 }
 
 amqp.connect('amqp://localhost', function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var ex = 'direct_logs';
+    var ex = 'topic_logs';
 
-    ch.assertExchange(ex, 'direct', {durable: false});
+    ch.assertExchange(ex, 'topic', {durable: false});
 
     ch.assertQueue('', {exclusive: true}, function(err, q) {
       console.log(' [*] Waiting for logs. To exit press CTRL+C');
 
-      args.forEach(function(severity) {
-        ch.bindQueue(q.queue, ex, severity);
+      args.forEach(function(key) {
+        ch.bindQueue(q.queue, ex, key);
       });
 
       ch.consume(q.queue, function(msg) {
-        console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
+        console.log(" [x] %s:'%s'", msg.fields.routingKey, msg.content.toString());
       }, {noAck: true});
     });
   });
